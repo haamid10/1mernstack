641ae163ca33b1758048774069fd7255d3ec5eff
haamid10 <apdixamiid919@gmail.com>
2022-05-29 14:15:15 +0300

    js basics

--- aTailwind/script.js
+++ aTailwind/script.js
@@ -22,10 +22,37 @@ let a=12
      counter++
      console.log(counter)
    }
-iner()
+return iner
   }
-  outter()
-  outter()
-  outter()
-  outter()
-  outter()
+  const nf= outter()
+ 
+ nf()
+ nf()
+
+
+//  function currying 
+
+function summ(a , b, c){
+  return a +b+c
+}
+console.log(summ(23,3,45))
+
+
+// 
+function curry(fn){
+  return function(a){
+    return function(b){
+      return function(c){
+        return fn(a, b, c)
+      }
+    }
+  }
+}
+
+const curriedSum =curry(summ)
+console.log (curriedSum(2)(3)(5))
+
+const add2 = curriedSum(2)
+const add3 = add2(3)
+ const add5 = add3(5)
+ console.log(add5)
