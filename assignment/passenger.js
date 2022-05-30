3e84edc8cf79bcbd620a53725903c81100388afa
haamid10 <apdixamiid919@gmail.com>
2022-05-30 13:45:37 +0300

    frecode academy

---
+++ assignment/passenger.js
@@ -0,0 +1,18 @@
+// alert("ssss")
+// document.getElementById("qaybta").innerText = 6
+
+// let count = 0
+
+// console.log(count)
+
+// let yAge =36
+
+// console.log(yAge)
+
+// let myage=23
+// let hummanDogRatio= 7
+
+// let count= myage * hummanDogRatio
+
+// console.log(count)
+
