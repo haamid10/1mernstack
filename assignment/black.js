26da3f0f4eb4fe47479e5578f3f7e8ecafd3eb92
haamid10 <apdixamiid919@gmail.com>
2022-06-07 20:58:57 +0300

    blackjack 1

--- assignment/black.js
+++ assignment/black.js
@@ -1,5 +1,15 @@
 let firstCard=9
-let SecondCard=6
+let SecondCard=8
 
-let Sum = firstCard + SecondCard
-console.log(Sum)
+let sum = firstCard + SecondCard
+console.log(sum)
+
+if(sum <= 20){
+  console.log("DO you want to draw a new card? 😊")
+} else if (sum === 21){
+  console.log( "whooo! You've got blackjack! 🤩 ")
+}else if(sum > 21){
+  console.log("You're out of the game! 😭 ")
+}
+ff
+// cash out your many!!
