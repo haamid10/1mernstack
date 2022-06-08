ef2c30690aadc97ede7b009054a832aa48a85447
haamid10 <apdixamiid919@gmail.com>
2022-06-08 22:46:20 +0300

    still grinding

--- assignment/black.js
+++ assignment/black.js
@@ -1,15 +1,25 @@
 let firstCard=9
-let SecondCard=8
-
+let SecondCard=12
 let sum = firstCard + SecondCard
-console.log(sum)
+let hasBlackJack= false
 
 if(sum <= 20){
   console.log("DO you want to draw a new card? 😊")
 } else if (sum === 21){
   console.log( "whooo! You've got blackjack! 🤩 ")
+  hasBlackJack=true
 }else if(sum > 21){
   console.log("You're out of the game! 😭 ")
 }
-ff
+
 // cash out your many!!
+console.log(hasBlackJack)
+
+
+// PREDICTIONS OF NUMBERS
+
+console.log(23 === 3 )
+console.log(23 >=3 )
+console.log(23 <=3 )
+console.log(23 <3 )
+console.log(23 <=23 )
